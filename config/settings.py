9541f6f7060f19933f8fa23dd93bf8b5845c3fba
grayroom <leejh9226@gmail.com>
2023-01-18 21:05:30 -0800

    fix static root

--- config/settings.py
+++ config/settings.py
@@ -162,7 +162,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'emr/static/'
+STATIC_URL = '/emr/static/'
+STATIC_ROOT = '/static'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'dist/emr/static'),
 ]
